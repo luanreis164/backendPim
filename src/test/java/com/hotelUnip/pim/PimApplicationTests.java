17e71c259f17c89e0f55e0c0f034ece937bbd3f6
Luan Reis <luanreis2202@gmail.com>
2021-10-02 20:28:54 -0300

    Criação do projeto

---
+++ src/test/java/com/hotelUnip/pim/PimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotelUnip.pim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
