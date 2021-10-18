90d90ec08ec248eed9287588add4467a8f081a36
Luan Reis <luanreis2202@gmail.com>
2021-10-18 15:22:09 -0300

    CRUD - Gerentes concluído

--- src/main/java/com/hotelUnip/pim/controllers/GerenteController.java
+++ src/main/java/com/hotelUnip/pim/controllers/GerenteController.java
@@ -1,13 +1,16 @@
 package com.hotelUnip.pim.controllers;
 import com.hotelUnip.pim.domain.Gerente;
+import com.hotelUnip.pim.dto.GerenteDTO;
+import com.hotelUnip.pim.dto.GerenteNewDTO;
 import com.hotelUnip.pim.services.GerenteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.validation.Valid;
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -28,6 +31,46 @@ public class GerenteController {
         return ResponseEntity.ok().body(lista);
 
     }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<Void> update(@Valid @RequestBody GerenteDTO objDto, @PathVariable Integer id){
+        Gerente obj = service.fromDto(objDto);
+        obj.setId(id);
+        obj = service.update(obj);
+        return ResponseEntity.noContent().build();
+
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Integer id){
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+
+    @GetMapping(value = "/page")
+    public ResponseEntity<Page<GerenteDTO>> findPage(@RequestParam(value = "page",defaultValue = "0") Integer page,
+                                                         @RequestParam(value = "linesPerPage",defaultValue = "24")Integer linesPerPage,
+                                                         @RequestParam(value = "orderBy",defaultValue = "nome")String orderBy,
+                                                         @RequestParam(value = "direction",defaultValue = "ASC")String direction){
+        Page<Gerente> list = service.findPage(page,linesPerPage,orderBy,direction);
+        Page<GerenteDTO> listDto = list.map(GerenteDTO::new);
+        return ResponseEntity.ok().body(listDto);
+
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> insert(@Valid @RequestBody GerenteNewDTO objDto){
+        Gerente obj = service.fromDto(objDto);
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
+
+    }
+
+
+
 
 
 
--- src/main/java/com/hotelUnip/pim/services/GerenteService.java
+++ src/main/java/com/hotelUnip/pim/services/GerenteService.java
@@ -1,9 +1,16 @@
 package com.hotelUnip.pim.services;
 
 import com.hotelUnip.pim.domain.Gerente;
+import com.hotelUnip.pim.dto.GerenteDTO;
+import com.hotelUnip.pim.dto.GerenteNewDTO;
 import com.hotelUnip.pim.repositories.GerenteRepository;
+import com.hotelUnip.pim.services.exceptions.DataIntegrityException;
 import com.hotelUnip.pim.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -26,6 +33,79 @@ public class GerenteService {
       return list;
 
     }
+
+    public Gerente insert(Gerente obj){
+        obj.setId(null);
+        try {  repo.save(obj);}
+        catch (DataIntegrityViolationException e){
+            throw new DataIntegrityException("Não é possivel cadastrar um email/cpf ja cadastrado!");
+
+        } return obj;
+    }
+
+
+
+    public Gerente update(Gerente obj){
+        Gerente newObj = find(obj.getId());
+        updateData(newObj,obj);
+        try {
+            return repo.save(newObj);
+        }
+        catch (DataIntegrityViolationException e){
+            throw new DataIntegrityException("Email ja atríbuido à uma conta! Tente com outro email.");
+        }
+    }
+
+
+    public void delete(Integer id){
+        find(id);
+        try {
+            repo.deleteById(id);
+
+        }
+        catch (DataIntegrityViolationException e ){
+            throw new DataIntegrityException("Não é possivel excluir um Gerente que tenha reserva!");
+        }
+
+    }
+
+    public Page<Gerente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+        PageRequest pageRequest = PageRequest.of(page,linesPerPage, Sort.Direction.valueOf(direction),orderBy);
+        return repo.findAll(pageRequest);
+    }
+
+
+    public Gerente fromDto(GerenteDTO objDto){
+        return new Gerente(objDto.getId(), objDto.getNome(),objDto.getEmail(),null,
+                objDto.getRua(),objDto.getBairro(),objDto.getNumero(),
+                objDto.getCep(),objDto.getCidade(),objDto.getEstado(), objDto.getTelefone(),null,null,objDto.getMatricula(),
+                objDto.getCtps(),objDto.getDataAdmissao(),objDto.getCargo(),objDto.getSalario(), objDto.getBonificacao());
+    }
+    public Gerente fromDto(GerenteNewDTO objDto){
+        return new Gerente(objDto.getId(), objDto.getNome(),objDto.getEmail(),objDto.getCpf(),
+                objDto.getRua(),objDto.getBairro(),objDto.getNumero(),
+                objDto.getCep(),objDto.getCidade(),objDto.getEstado(), objDto.getTelefone(), objDto.getRg(), objDto.getDataNasc(),objDto.getMatricula(),
+                objDto.getCtps(),objDto.getDataAdmissao(),objDto.getCargo(),objDto.getSalario(), objDto.getBonificacao());
+    }
+
+    private void updateData(Gerente newObj, Gerente obj){
+        newObj.setNome(obj.getNome());
+        newObj.setEmail(obj.getEmail());
+        newObj.setRua(obj.getRua());
+        newObj.setBairro(obj.getBairro());
+        newObj.setNumero(obj.getNumero());
+        newObj.setCep(obj.getCep());
+        newObj.setCidade(obj.getCidade());
+        newObj.setEstado(obj.getEstado());
+        newObj.setTelefone(obj.getTelefone());
+        newObj.setMatricula(obj.getMatricula());
+        newObj.setCtps(obj.getCtps());
+        newObj.setDataAdmissao(obj.getDataAdmissao());
+        newObj.setCargo(obj.getCargo());
+        newObj.setSalario(obj.getSalario());
+        newObj.setBonificacao(obj.getBonificacao());
+    }
+
 
 
 }
